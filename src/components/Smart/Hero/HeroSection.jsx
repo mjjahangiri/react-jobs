db8ed35858f79636c0d589908ec02738d080243c
MJJ <mj.jahangiri.6@gmail.com>
2024-09-02 20:24:02 +0330

    feat: add hero section for project

---
+++ src/components/Smart/Hero/HeroSection.jsx
@@ -0,0 +1,12 @@
+import "./HeroSection.scss";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-wrapper">
+      <h1 className="title">Become a React Dev</h1>
+      <p className="description">Find a React job that fits your skill set</p>
+    </div>
+  );
+};
+
+export default HeroSection;
