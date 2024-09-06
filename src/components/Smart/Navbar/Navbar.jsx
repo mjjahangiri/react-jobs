72d0d08db395134ce2da930eae662a811f0eb92d
MJJ <mj.jahangiri.6@gmail.com>
2024-09-06 23:31:32 +0330

    refactor: change link to navLink in navbar component

--- src/components/Smart/Navbar/Navbar.jsx
+++ src/components/Smart/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.scss";
 import reactLogo from "../../../assets/react.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -7,26 +8,36 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="brand-wrapper">
           <img src={reactLogo} alt="reactLogo" className="brand-logo" />
-          <a className="brand" href="#">
+          <NavLink className="brand" to="/">
             React Jobs
-          </a>
+          </NavLink>
         </div>
 
         <ul className="nav">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Active
-            </a>
+            <NavLink
+              className={`nav-link ${(isActive) => (isActive ? "active" : "")}`}
+              aria-current="page"
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
+            <NavLink
+              className={`nav-link ${(isActive) => (isActive ? "active" : "")}`}
+              to="/jobs"
+            >
+              Jobs
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
+            <NavLink
+              className={`nav-link ${(isActive) => (isActive ? "active" : "")}`}
+              to="/add-job"
+            >
+              Add Job
+            </NavLink>
           </li>
         </ul>
       </nav>
